11f4d05e09404eca525d95a48e595828e102b2db
khoale-groove <khoa.lena@groovetechnology.com>
2024-02-29 13:29:19 +0700

    init

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
+declare const global: any; // Add this line to explicitly define the type of the global object
+
 export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -1,4 +1,4 @@
-import { prisma } from ".";
+import { prisma } from "./client";
 
 import type { User } from "@prisma/client";
 
@@ -24,8 +24,8 @@ const DEFAULT_USERS = [
           create: {
             ...user,
           },
-        })
-      )
+        }),
+      ),
     );
   } catch (error) {
     console.error(error);
