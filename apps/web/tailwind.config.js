996d1791501c29ae6d2f5df49423bd2893ec7b38
khoale-groove <khoa.lena@groovetechnology.com>
2024-02-29 13:46:38 +0700

    use yarn version 4.1.0

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,14 @@
+const { nextui } = require("@nextui-org/react")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{html,js,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
-
